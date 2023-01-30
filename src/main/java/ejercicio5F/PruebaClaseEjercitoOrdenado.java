683fbdd44012d2683d121720cefe5683fa4c8599
tomasgnzlz <tgonati327@g.educaand.es>
2023-01-30 13:53:06 +0100

    mfalto

--- src/main/java/ejercicio5F/PruebaClaseEjercitoOrdenado.java
+++ src/main/java/ejercicio5F/PruebaClaseEjercitoOrdenado.java
@@ -24,20 +24,20 @@ public class PruebaClaseEjercitoOrdenado {
         //Se crean los Soldados. 
         Soldado s1 = new Soldado(1, "Tomas", "Gonzalez", "Atienza", 19);
         Soldado s2 = new Soldado(2, "Ariel", "Gonzalez", "Atienza", 19);
-        Soldado s3 = new Soldado(3, "Juan", "Hernandes", "Messi", 29);
-        Soldado s4 = new Soldado(4, "Marta", "Morales", "Gonzalez", 39);
         Soldado s5 = new Soldado(5, "Juan", "Herrera", "Martines", 49);
         Soldado s6 = new Soldado(6, "Carlos", "Navarro", "Herrera", 59);
         Soldado s7 = new Soldado(7, "Antonio", "Martines", "Navarro", 69);
+        Soldado s3 = new Soldado(3, "Juan", "Hernandes", "Messi", 29);
+        Soldado s4 = new Soldado(4, "Marta", "Morales", "Gonzalez", 39);
 
         //Uso del método para alistar soldados y añadirlos a la listaSoldadosAlistados. 
         ejercito.alistarSoldado(s1);
         ejercito.alistarSoldado(s2);
-        ejercito.alistarSoldado(s3);
         ejercito.alistarSoldado(s4);
         ejercito.alistarSoldado(s5);
         ejercito.alistarSoldado(s6);
         ejercito.alistarSoldado(s7);
+        ejercito.alistarSoldado(s3);
 
         //Uso del método nº de soldados que hay en el ejercito.
         int numSoldados = ejercito.numeroSoldados();
@@ -63,19 +63,17 @@ public class PruebaClaseEjercitoOrdenado {
         System.out.println(" ");
         //Si sale un soldado menos que en el método anterior quiere decir que nuestro metodo funciona. 
         System.out.println("El ejercito tiene " + numSoldados + " alistados." + "\n");
-        
-        
-//································································································································
 
+//································································································································
         Soldado s8 = new Soldado(7, "Antonio", "Martines", "Navarro", 69);
         ejercito.alistarSoldado(s8);
         //Imprimo la lista de soldados
         ejercito.getEjercito().forEach(System.out::println);
         //Como el solado con ese nombre ya está alistado no lo añade a la lista de soldados. 
-        
+
         /*
         ME FALTA ORDENARLO CON EL COMPARATOR QUE SERÍA EL ORDEN NATURAL DE LOS SET. 
-        */
+         */
     }
-    
+
 }
