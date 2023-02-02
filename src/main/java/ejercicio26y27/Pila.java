f150da48e5dcc65f046f4d50fe5ac591b8d7ac01
tomasgnzlz <tgonati327@g.educaand.es>
2023-02-02 11:07:23 +0100

    Ejercicio 26 y 27 de la relación del tema 5

---
+++ src/main/java/ejercicio26y27/Pila.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio26y27;
+
+import java.util.List;
+
+/**
+ *
+ * @author tomas
+ */
+public class Pila<E> {
+
+    protected List<E> listaGenerica;
+
+    //Constructor que cree las pilas. 
+    public Pila(/*Le quito lo que tiene por defecto para que al crearla siempre slaga vacio*/) {
+        this.listaGenerica = listaGenerica;
+    }
+
+}
