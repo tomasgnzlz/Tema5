67719cdece31ef467392b0d88dfd72d67fbff539
tomasgnzlz <tgonati327@g.educaand.es>
2023-02-02 12:45:57 +0100

    Añadir tamaño a la clase pila y al constructor, hay que darle el tamaño en el constructor al crear la pila

--- src/main/java/PreExamen/Prueba.java
+++ src/main/java/PreExamen/Prueba.java
@@ -14,7 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Ejercicio26&27 Tema 5.
+        
     }
-    
 }
--- src/main/java/ejercicio26y27/Pila.java
+++ src/main/java/ejercicio26y27/Pila.java
@@ -11,26 +11,59 @@ import java.util.List;
  *
  * @author tomas
  */
-public class Pila<E> {
+public class Pila<T> { //La T recibe cualquier tipo de objeto. 
 
-     private List<E> listaGenerica;
+    private List<T> listaGenerica;
 
-//  A)Constructor que cree las pilas. 
+    //A)Constructor que cree las pilas. 
     public Pila(/*Le quito lo que tiene por defecto para que al crearla siempre slaga vacio*/) {
         this.listaGenerica = new ArrayList<>();
     }
-    
-//  B)
-    
-    
+
+    //B)Método para apilar elemento, lo insertará donde proceda. 
+    public void push(T elemento) { //Push = apilar/AñadirElementos.
+        listaGenerica.add(elemento);
+    }
+
+    //C)Método para desapilar elemento de donde proceda. 
+    public void pop(T elemento) {
+        listaGenerica.remove(elemento);
+    }
+
     //D)Método para saber si la pila está vacía o no. 
-    public boolean estaVacia(){
+    public boolean estaVacia() {
         return this.listaGenerica.isEmpty();
     }
+
+    //E)Método para saber si la pila está llena o no.
+    public boolean estaLlena(int numeroElementos, int Capacidad) {
+        //La capacidad de pilas se la dices tu. 
+        boolean estaLlena = false;
+        if (numeroElementos == Capacidad) {
+            estaLlena = true;
+        } else {
+            estaLlena = false;
+        }
+        return estaLlena;
+
+    }
+
+    //F)Método para saber el numero de lementos que tiene la pila. 
+    public int numeroelementos() {
+        return this.listaGenerica.size();
+    }
+
+    //G)Método para mostrar los elementos con el orden en el que se han creado.
+    public List<T> mostrarLista() {//Puedo poner ArrayList<T> o simplemente List<T>.
+        List<T> lista = new ArrayList<>();
+
+        for (T elemento : listaGenerica) {
+            //System.out.println(elemento);
+            lista.add(elemento);
+        }
+        return lista;
+    }
     
-    
-    
-    
-    
-    
+    //H)
+
 }
