1b196353302dece10ac6478063e6e46802bb1462
tomas_gnzlz <tgonati327@g.educaand.es>
2023-02-05 21:26:04 +0100

    NOMESALE.

--- src/main/java/ejercicio26y27/MainPila.java
+++ src/main/java/ejercicio26y27/MainPila.java
@@ -10,27 +10,25 @@ package ejercicio26y27;
  */
 public class MainPila {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Se crea la pila en cuestión
-        Pila p1 = new Pila();
+        Pila<String> stringPila = new Pila(5);
         
         //Prueba del Método para añadir elementos a la pila.
-        p1.push(1);
-        p1.push(5);
-        p1.push(3);
-        p1.push(7);
-        p1.push(9);
-        System.out.println("Elementos de la lista: "+p1.mostrarLista());
+        stringPila.push("1");
+        stringPila.push("5");
+        stringPila.push("3");
+        stringPila.push("7");
+        stringPila.push("9");
+        stringPila.mostrarListaPila();
         
         //Prueba del Método para eliminar elementos de la pila. 
-        p1.pop(7);
-        System.out.println("Elementos de la lista tras eliminar el elemento 7: " + p1.mostrarLista());
-        System.out.println("La lista tiene "+p1.numeroelementos() + " elementos");
-        p1.push(6);//Este elemento debería de salir el primero de la lista.
-        System.out.println("Una vez añadimos el elemento 6, este deberia de salir el primero en la lista " + p1.mostrarLista());
-        //TERMINAR
+        stringPila.pop("7");
+        System.out.println("Elementos de la lista tras eliminar el elemento 7: ");
+        stringPila.mostrarListaPila();
+        System.out.println("La lista tiene "+stringPila.numeroelementos() + " elementos");
+        stringPila.push("6");//Este elemento debería de salir el primero de la lista.
+        System.out.println("Una vez añadimos el elemento 6, este deberia de salir el primero en la lista ");
+        stringPila.mostrarListaPila();
     }    
 }
--- src/main/java/ejercicio26y27/Pila.java
+++ src/main/java/ejercicio26y27/Pila.java
@@ -4,6 +4,7 @@
  */
 package ejercicio26y27;
 
+import java.rmi.server.ObjID;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +15,13 @@ import java.util.List;
 public class Pila<T> { //La T recibe cualquier tipo de objeto. 
 
     private List<T> listaGenerica;
+    private int tamanio;
     //METER EL ATRIBUTO PILA.
     //A)Constructor que cree las pilas. 
-    public Pila(/*Le quito lo que tiene por defecto para que al crearla siempre slaga vacio*/) {
+    public Pila(int tamanio/*Le quito lo que tiene por defecto para que al crearla siempre slaga vacio*/) {
+        tamanio = Math.abs(tamanio);
         this.listaGenerica = new ArrayList<>();
+        this.tamanio = tamanio;
     }
 
     //B)Método para apilar elemento, lo insertará donde proceda. 
@@ -54,16 +58,39 @@ public class Pila<T> { //La T recibe cualquier tipo de objeto.
     }
 
     //G)Método para mostrar los elementos con el orden en el que se han creado.
-    public List<T> mostrarLista() {//Puedo poner ArrayList<T> o simplemente List<T>.
-        List<T> lista = new ArrayList<>();
-
-        for (T elemento : listaGenerica) {
-            //System.out.println(elemento);
-            lista.add(elemento);
+//    public List<T> mostrarLista() {//Puedo poner ArrayList<T> o simplemente List<T>.
+//        List<T> lista = new ArrayList<>();
+//
+//        for (T elemento : listaGenerica) {
+//            //System.out.println(elemento);
+//            lista.add(elemento);
+//        }NO VALE
+//        return lista;
+//    }
+    //G)Método para mostrar los elementos con el orden en el que se han creado.
+    public void mostrarListaPila(){ //Recibe un array cualquiera/Genérico. 
+        for (int i = listaGenerica.size() - 1; i < 0; i++) {
+            System.out.println(listaGenerica.get(i));
         }
-        return lista;
     }
     
-    //H)
+    //H)Método que  almacena en la pila...¿?
+    public void rellenarArray( T [] aux){
+        int numElementos = aux.length;
+        if (numElementos <= tamanio ) {
+            for (int i = 0; i < numElementos; i++) {
+                push(aux[i]);
+            }
+        }else{
+            System.out.println("***");
+        }
+    }
+    
+    //G) Método para sacar elementos de la pila. 
+    public Object[] sacarElementosPila(){
+        Object[] elementos = this.listaGenerica.toArray();
+        this.listaGenerica.clear();
+        return elementos;
+    }
 
 }
